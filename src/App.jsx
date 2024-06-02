fb77a1e0ef7762629ac41dfc21135238fdfc3f3d
Kaustubh-404 <kaustubhp2003@gmail.com>
2024-06-02 17:06:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home' 
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignOut from './pages/SignOut'
+import SignIn from './pages/SignIn'
+
+
+const App = () => {
+  return <BrowserRouter>
+  <Routes>
+
+  <Route path='/' element={<Home />}/>
+  <Route path='/profile' element={<Profile />}/>
+  <Route path='/about' element={<About />}/>
+  <Route path='/signin' element={<SignIn />}/>
+  <Route path='/signout' element={<SignOut />}/>
+
+  </Routes>
+  </BrowserRouter>
+}
+
+export default App
