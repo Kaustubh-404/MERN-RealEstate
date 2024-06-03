fe7f2996bc6355d6108fa33e9efa72439e825524
Kaustubh-404 <kaustubhp2003@gmail.com>
2024-06-03 13:36:55 +0530

    minor change in clientside

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home' 
 import Profile from './pages/Profile'
 import About from './pages/About'
-import SignOut from './pages/SignOut'
+import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Header from './components/Header'
 
@@ -19,7 +19,7 @@ const App = () => {
   <Route path='/profile' element={<Profile />}/>
   <Route path='/about' element={<About />}/>
   <Route path='/signin' element={<SignIn />}/>
-  <Route path='/signout' element={<SignOut />}/>
+  <Route path='/signup' element={<SignUp />}/>
 
   </Routes>
   </BrowserRouter>
