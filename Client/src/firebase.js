ff9385b533ba39cb64f3cfc7f214af22d18b40f5
Kaustubh-404 <kaustubhp2003@gmail.com>
2024-06-16 13:58:15 +0530

    update firebase.js

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,//"AIzaSyAg9E_GirUn2ma9IDksntSG3HRHZk5-5f0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-realestate-3da80.firebaseapp.com",
   projectId: "mern-realestate-3da80",
   storageBucket: "mern-realestate-3da80.appspot.com",
