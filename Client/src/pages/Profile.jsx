d824499f3c56c6b7e4e27405702880ada0710bcd
Kaustubh-404 <kaustubhp2003@gmail.com>
2024-06-16 15:07:51 +0530

    complete profile page UI

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const {currentUser} = useSelector(state => state.user)
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img className='self-center mt-2 rounded-full h-24 w-24 object-cover cursor-pointer  ' src={currentUser.avatar} alt='profile photo'/>
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' />
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' />
+        <input type='text' placeholder='password' className='border p-3 rounded-lg' id='password' />
+
+        <button className='bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80'>Update</button>
+
+      </form>
+
+      <div className='flex mt-5 justify-between'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+
+    </div>
   )
 }
 
