ca57a7f8e9fe858156ab4822a4147cf4abe838f3
Kaustubh-404 <kaustubhp2003@gmail.com>
2024-06-18 13:31:36 +0530

    complete image upload functionality

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,13 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
+import { app } from '../firebase.js';
+import {getDownloadURL, getStorage , ref, uploadBytesResumable} from 'firebase/storage';
 
 const Profile = () => {
+  const fileRef = useRef(null)
   const {currentUser} = useSelector(state => state.user)
+  const [file , setFile] = useState(undefined)
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  // console.log(formData);
+  // console.log(filePerc);
+  // console.log(fileUploadError);
+  // console.log(downloadURL);
+
+  
+
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed', 
+      (snapshot) => {
+        const progress = 
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then
+        ((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
+
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img className='self-center mt-2 rounded-full h-24 w-24 object-cover cursor-pointer  ' src={currentUser.avatar} alt='profile photo'/>
+        <input onChange={(e)=>setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/.*'/>
+        <img onClick={()=>fileRef.current.click()} className='self-center mt-2 rounded-full h-24 w-24 object-cover cursor-pointer ' 
+        src={formData.avatar || currentUser.avatar} alt='profile photo'/>
+
+
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
+        </p>
+        
+
         <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' />
         <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' />
         <input type='text' placeholder='password' className='border p-3 rounded-lg' id='password' />
@@ -26,3 +94,65 @@ const Profile = () => {
 }
 
 export default Profile
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const handleFileUpload = () => {
+  //   const storage = getStorage(app);
+  //   const fileName = new Date().getTime() + file.name;
+  //   const storageRef = ref(storage, fileName)
+  //   const uploadTask = uploadBytesResumable(storageRef, file);
+
+  //   uploadTask.on('state_changed',
+  //     (snapshot) => {
+  //       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  //       setFilePerc(Math.round(progress));
+  //       console.log('Upload is '+ progress +'% done')
+  //     });
+  //     // (error) => {
+  //     //   setFileUploadError(true);   
+  //     // };
+  //     // ()=>{
+  //     //   getDownloadURL(uploadTask.snapshot.ref).then
+  //     //   ((downloadURL)=>{
+  //     //      setFormData({...formData, avatar:downloadURL});
+  //     //   });
+  //     // }
+  // };
